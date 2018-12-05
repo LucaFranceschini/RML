ab85b33fed68278a3e9f7a4c3b15788517742b0c
Luca Franceschini <luca.franceschini92@gmail.com>
2018-12-05 12:53:45 +0100

    support Kotlin

---
+++ compiler/src/main/kotlin/rml/compiler/Compiler.kt
@@ -0,0 +1,20 @@
+package rml.compiler
+
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.RecognitionException
+import java.io.IOException
+
+fun main(args: Array<String>) {
+    try {
+        val input = CharStreams.fromStream(System.`in`)
+        val lexer = rmlLexer(input)
+        val tokenStream = CommonTokenStream(lexer)
+        val parser = rmlParser(tokenStream)
+        println(parser.spec().payload.text)
+    } catch (e: IOException) {
+        System.err.println(e.message)
+    } catch (e: RecognitionException) {
+        System.err.println(e.message)
+    }
+}
