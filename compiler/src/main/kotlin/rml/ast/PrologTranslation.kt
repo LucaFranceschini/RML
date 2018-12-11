631d43c9259f2d030888fb668510559f70ce5bd0
Luca Franceschini <luca.franceschini92@gmail.com>
2018-12-11 16:57:55 +0100

    fixed variable translation

--- compiler/src/main/kotlin/rml/ast/PrologTranslation.kt
+++ compiler/src/main/kotlin/rml/ast/PrologTranslation.kt
@@ -20,9 +20,7 @@ fun toProlog(traceExp: TraceExp): PrologTerm = when(traceExp) {
     is BlockTraceExp -> FunctionTerm("var",
             ListTerm(traceExp.declaredVars.map { FunctionTerm.constant(it.name) }.toList()),
             toProlog(traceExp.traceExp))
-    is TraceExpVar -> FunctionTerm("app",
-            VarTerm(traceExp.id.name),
-            ListTerm(traceExp.genericVars.map { FunctionTerm.constant(it.name) }.toList()))
+    is TraceExpVar -> toProlog(traceExp)
     is EventTypeTraceExp -> FunctionTerm(traceExp.id.name, traceExp.eventTerms.map { toProlog(it) }.toList())
     is ConcatTraceExp -> toProlog(traceExp, "*")
     is AndTraceExp -> toProlog(traceExp, "/\\")
@@ -32,6 +30,16 @@ fun toProlog(traceExp: TraceExp): PrologTerm = when(traceExp) {
 
 fun toProlog(traceExp: BinaryTraceExp, opSymbol: String): PrologTerm =
         FunctionTerm(opSymbol, toProlog(traceExp.left), toProlog(traceExp.right))
+
+fun toProlog(traceExp: TraceExpVar): PrologTerm {
+    val variable = VarTerm(traceExp.id.name)
+
+    if (traceExp.genericVars.isEmpty())
+        return variable
+
+    return FunctionTerm("app", variable,
+            ListTerm(traceExp.genericVars.map { FunctionTerm.constant(it.name) }.toList()))
+}
 
 fun toProlog(eventTerm: EventTerm): PrologTerm = when(eventTerm) {
     is VarEventTerm -> VarTerm(eventTerm.id.name)
