75a88ab2f543cf686cb07c2d2565542bff8e37a2
Luca Franceschini <luca.franceschini92@gmail.com>
2018-12-18 12:22:56 +0100

    spaces

--- compiler/src/main/kotlin/rml/ast/Identifiers.kt
+++ compiler/src/main/kotlin/rml/ast/Identifiers.kt
@@ -13,7 +13,7 @@ abstract class AbstractId(name: String) {
 }
 
 // variables from parametric and generic trace expressions
-data class VarId(val name: String) : AbstractId(name)
+data class VarId(val name: String): AbstractId(name)
 
 // variables representing trace expression eventTerms
-data class TraceExpId(val name: String) : AbstractId(name)
+data class TraceExpId(val name: String): AbstractId(name)
