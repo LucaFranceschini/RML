63ed2eaad611ac7a1a757b5b112bec7380c3b7cd
Luca Franceschini <luca.franceschini92@gmail.com>
2019-01-08 09:57:18 +0100

    AST support for list values

--- compiler/src/main/kotlin/rml/ast/EventTypes.kt
+++ compiler/src/main/kotlin/rml/ast/EventTypes.kt
@@ -1,12 +1,15 @@
 package rml.ast
 
 sealed class DataValue
+
 sealed class SimpleValue: DataValue()
 data class StringValue(val string: String): SimpleValue()
 data class IntValue(val number: Int): SimpleValue()
 data class VarValue(val id: VarId): SimpleValue() {
     constructor(id: String): this(VarId(id))
 }
+data class ListSimpleValue(val values: List<SimpleValue>): SimpleValue()
+
 data class ObjectValue(val fields: List<Field>): DataValue() {
     data class Field(val key: KeyId, val value: DataValue) {
         constructor(key: String, value: DataValue): this(KeyId(key), value)
@@ -16,6 +19,8 @@ data class ObjectValue(val fields: List<Field>): DataValue() {
         require(fields.isNotEmpty()) { "Non-empty object expected" }
     }
 }
+
+data class ListValue(val values: List<DataValue>): DataValue()
 
 sealed class EvtypeDecl(open val evtype: EventType)
 data class DirectEvtypeDecl(override val evtype: EventType, val objects: List<ObjectValue>): EvtypeDecl(evtype) {
