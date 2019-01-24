4441c2ea456d5e98c7de7604b3cef3268810a55c
Luca Franceschini <luca.franceschini92@gmail.com>
2019-01-24 11:12:15 +0100

    fix relative paths

--- instrumentation/nodejs/instrument.sh
+++ instrumentation/nodejs/instrument.sh
@@ -13,6 +13,12 @@ if [ $# -ne 2 ]
 		exit 1
 fi
 
+# convert relative path to absolute
+function realpath { echo $(cd $(dirname $1); pwd)/$(basename $1); }
+
+logFile=$(realpath "$1")
+program=$(realpath "$2")
+
 # instrument & go
 DIR="$PWD"/`dirname "$0"`
-npm run --prefix="$DIR" --silent jalangi -- --initParam log:"$PWD/$1" --inlineIID --inlineSource --analysis functionInvocationAnalysis.js "$PWD/$2" "${@:3}"
+npm run --prefix="$DIR" --silent jalangi -- --initParam log:"$logFile" --inlineIID --inlineSource --analysis functionInvocationAnalysis.js "$program" "${@:3}"
