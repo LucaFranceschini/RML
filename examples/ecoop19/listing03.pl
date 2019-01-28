b3774308c2d4eeedf5b80d5ce2924b6571c626b8
Luca Franceschini <luca.franceschini92@gmail.com>
2019-01-28 20:06:02 +0100

    don't version Prolog files, they keep changing

--- examples/ecoop19/listing03.pl
+++
@@ -1,9 +0,0 @@
-:- module('spec', [trace_expression/2, match/2]).
-:- use_module(monitor('deep_subdict')).
-match(Event, hasNext(Res)) :- deep_subdict(_{'result':Res,'name':"hasNext",'event':"func_post"}, Event).
-match(Event, next) :- deep_subdict(_{'name':"next",'event':"func_pre"}, Event).
-match(Event, nonStruct) :- deep_subdict(_{'name':"contains",'event':"func_pre"}, Event).
-match(Event, nonStruct) :- deep_subdict(_{'name':"containsAll",'event':"func_pre"}, Event).
-match(Event, notIt) :- not(match(Event, hasNext(_))), not(match(Event, next)).
-match(Event, notCol) :- not(match(Event, nonStruct)), not(match(Event, hasNext('false'))).
-trace_expression('NotIt', NotIt) :- NotIt=(eps\/(notIt:NotIt)), NotCol=(eps\/(notCol:NotCol)), NonStruct=(eps\/(nonStruct:NonStruct)), Collection=(NonStruct*(hasNext('false'):eps)), Iterator=((hasNext('false'):eps)\/(hasNext('true'):(next:Iterator))), Main=((Iterator|NotIt)/\(Collection|NotCol)).
