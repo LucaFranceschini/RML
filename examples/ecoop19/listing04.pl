1840b0d0f0616053f1f94a3f2ccad75d6f7bc8e1
davideancona <davide.ancona@unige.it>
2019-01-28 09:34:40 +0100

    ecoop19 paper examples

--- examples/ecoop19/listing04.pl
+++ examples/ecoop19/listing04.pl
@@ -4,8 +4,6 @@ match(Event, hasNext(Res)) :- deep_subdict(_{'result':Res,'name':"hasNext",'even
 match(Event, next) :- deep_subdict(_{'name':"next",'event':"func_pre"}, Event).
 match(Event, nonStruct) :- deep_subdict(_{'name':"contains",'event':"func_pre"}, Event).
 match(Event, nonStruct) :- deep_subdict(_{'name':"containsAll",'event':"func_pre"}, Event).
-match(Event, notIt) :- not(match(Event, hasNext(_))).
-match(Event, notIt) :- not(match(Event, next)).
-match(Event, notCol) :- not(match(Event, nonStruct)).
-match(Event, notCol) :- not(match(Event, hasNext(var(false)))).
+match(Event, notIt) :- not(match(Event, hasNext(_))), not(match(Event, next)).
+match(Event, notCol) :- not(match(Event, nonStruct)), not(match(Event, hasNext(var(false)))).
 trace_expression('Collection', Collection) :- Collection=(nonStruct:(hasNext(var(false)):eps)), Iterator=(hasNext(var(false)):(rop:(hasNext(var(true)):(next:Iterator)))), Main=((Iterator*(huffleop:(notIt:eps)))*(ndop:(Collection*(huffleop:(notCol:eps))))).
