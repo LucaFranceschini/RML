f79418eb99f458e9ae5ed94bf553689ea16d3cd9
davideancona <davide.ancona@unige.it>
2019-02-19 09:33:41 +0100

    examples

--- examples/iot/temperature01.pl
+++ examples/iot/temperature01.pl
@@ -2,4 +2,4 @@
 :- use_module(monitor('deep_subdict')).
 match(Event, temp_sensor(Temp)) :- deep_subdict(_{'temperature':Temp}, Event).
 match(_, any).
-trace_expression('Main', Main) :- Main=app(CheckTemp, [0]), CheckTemp=gen(['temp'], guarded((((var('temp')>=10),(var('temp')=<50))), var(temp, (temp_sensor(var(temp)):(eps\/app(CheckTemp, [var('temp')])))), 0)).
+trace_expression('Main', Main) :- Main=app(CheckTemp, [20]), CheckTemp=gen(['temp'], guarded((((var('temp')>=10),(var('temp')=<50))), var(temp, (temp_sensor(var(temp)):(eps\/app(CheckTemp, [var('temp')])))), 0)).
