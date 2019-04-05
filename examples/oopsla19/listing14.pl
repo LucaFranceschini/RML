e657d62414581c723216824733568627a2fcff8d
davideancona <davide.ancona@unige.it>
2019-04-05 16:56:17 +0200

    examples

--- examples/oopsla19/listing14.pl
+++ examples/oopsla19/listing14.pl
@@ -1,12 +1,6 @@
 :- module('spec', [trace_expression/2, match/2]).
 :- use_module(monitor('deep_subdict')).
-match(Event, acquire(Id)) :- deep_subdict(_{'result':Id,'name':"fs.open",'event':"func_post"}, Event).
-match(Event, release(Id)) :- deep_subdict(_{'args':[Id],'name':"fs.close",'event':"func_pre"}, Event).
-match(Event, use(Id)) :- deep_subdict(_{'args':[var(id)|_],'name':"fs.read",'event':"func_pre"}, Event).
-match(Event, use(Id)) :- deep_subdict(_{'args':[var(id)|_],'name':"fs.write",'event':"func_pre"}, Event).
-match(Event, use(Id)) :- deep_subdict(_{'args':[var(id)|_],'name':"fs.fchmod",'event':"func_pre"}, Event).
-match(Event, event(Id)) :- match(Event, acquire(var(id))).
-match(Event, event(Id)) :- match(Event, use(var(id))).
-match(Event, event(Id)) :- match(Event, release(var(id))).
+match(Event, push(Val)) :- deep_subdict(_{'args':[Val],'name':"push",'event':"func_pre"}, Event).
+match(Event, pop(Val)) :- deep_subdict(_{'result':Val,'name':"pop",'event':"func_post"}, Event).
 match(_, any).
-trace_expression('Main', Main) :- Main=optional(var(id, (acquire(var(id)):(app(Resource, [var('id')])/\Main)))), Resource=gen(['id'], (((event(var(id))>>(star(use(var(id)))*(release(var(id)):1)));1))).
+trace_expression('Main', Main) :- Main=optional(var(val, (push(var(val)):(Main*(pop(var(val)):Main))))).
