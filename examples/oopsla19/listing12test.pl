092722e3d34493dd216e46fd667d2860e7aab126
davideancona <davide.ancona@unige.it>
2019-04-10 12:31:04 +0200

    examples

--- examples/oopsla19/listing12test.pl
+++ examples/oopsla19/listing12test.pl
@@ -3,11 +3,13 @@
 match(Event, acquire(Id)) :- deep_subdict(_{'targetId':Id,'name':"acquire",'event':"func_pre"}, Event).
 match(Event, release(Id)) :- deep_subdict(_{'targetId':Id,'name':"release",'event':"func_pre"}, Event).
 match(Event, use(Id)) :- deep_subdict(_{'targetId':Id,'name':"use",'event':"func_pre"}, Event).
-match(Event, event(Id)) :- match(Event, acquire(Id)).
-match(Event, event(Id)) :- match(Event, use(Id)).
-match(Event, event(Id)) :- match(Event, release(Id)).
+match(Event, event(Id)) :- match(Event, acquire(var(id))).
+match(Event, event(Id)) :- match(Event, use(var(id))).
+match(Event, event(Id)) :- match(Event, release(var(id))).
+match(Event, acqRel(Id)) :- match(Event, acquire(var(id))).
+match(Event, acqRel(Id)) :- match(Event, release(var(id))).
 match(Event, filter) :- match(Event, acquire(_)).
 match(Event, filter) :- match(Event, release(_)).
 match(Event, filter) :- match(Event, use(_)).
 match(_, any).
-trace_expression('Main', Main) :- Main=(((filter>>optional(var(id, (acquire(var(id)):(app(Resource, [var('id')])/\Main)))));1)), Resource=gen(['id'], (((event(var(id))>>(star(use(var(id)))*(release(var(id)):1)));1))).
+trace_expression('Main', Main) :- Main=optional(var(id, (acquire(var(id)):((star((Main|use(var(id))))*(release(var(id)):eps))/\(((acqRel(var(id))>>(release(var(id)):1));1)))))).
