4b5d9521601b058835b35f3e4a0f4b931ef9d037
davideancona <davide.ancona@unige.it>
2019-04-24 11:54:38 +0200

    examples

---
+++ examples/oopsla19/quantitave.js
@@ -0,0 +1,24 @@
+// generator of correct traces for listing12.rml: exclusive access to multiple resources
+
+'use strict';
+
+var trace_size = process.argv[2] || 100;
+var total_res = 1000000;
+var curr_res = total_res;
+var used = Math.floor(total_res/trace_size);
+
+function available() {
+    console.log('available('+total_res+')');
+    return total_res;
+}
+
+function use(total) {
+    console.log('use('+total+','+used+')');
+    curr_res -= used;
+    return used;
+}
+
+available();
+for (var event_counter = 1; event_counter <= trace_size; event_counter++) {
+    use(curr_res,used);
+}
