772efcaa0ab179ed9ce3f841bb3da51e7fd20df5
davideancona <davide.ancona@unige.it>
2019-04-24 12:12:30 +0200

    examples

---
+++ examples/oopsla19/listing13test.pl
@@ -0,0 +1,8 @@
+:- module('spec', [trace_expression/2, match/2]).
+:- use_module(monitor('deep_subdict')).
+match(Event, available(Total)) :- deep_subdict(_{'res':Total,'name':"available",'event':"func_post"}, Event).
+match(Event, use(Total, Used)) :- deep_subdict(_{'args':[Total, Used],'name':"use",'event':"func_pre"}, Event).
+match(Event, filter) :- match(Event, available(_)).
+match(Event, filter) :- match(Event, use(_, _)).
+match(_, any).
+trace_expression('Main', Main) :- Use=gen(['total'], guarded((var('total')>0), var(used, (use(var(total), var(used)):app(Use, [(var('total')-var('used'))]))), eps)), Main=(((filter>>var(total, clos((available(var(total)):app(Use, [var('total')])))));1)).
