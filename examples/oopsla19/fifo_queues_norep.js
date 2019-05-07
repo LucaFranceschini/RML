f6a30bc723b7f3ddd01001d0a62ddaf96d136700
davideancona <davide.ancona@unige.it>
2019-05-07 17:14:39 +0200

    examples

---
+++ examples/oopsla19/fifo_queues_norep.js
@@ -0,0 +1,36 @@
+// generator of correct traces for fifo_queues.rml: FIFO queues with repetitions
+
+'use strict';
+
+var trace_size = process.argv[2] || 100;
+var max_size = Math.floor(trace_size/10);
+var max_elem = 10;
+var queue = [];
+
+function getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function dequeue() {
+    var el=queue.pop();
+    console.log('dequeue(' + el + ')');
+    return el;
+}
+
+function enqueue(el) {
+    console.log('enqueue(' + el + ')');
+    if(queue.includes(el))
+	return;
+    queue.unshift(el);
+}
+
+for (var event_counter = 1; event_counter <= trace_size; event_counter++) {
+    if (queue.length==0)
+	enqueue(getRandom(max_elem));
+    else if (queue.length==max_size)
+	dequeue();
+    else
+        getRandom(max_size)>=queue.length?enqueue(getRandom(max_elem)):dequeue();
+}
+while(queue.length)
+    dequeue();
---
+++ examples/oopsla19/fifo_queues_noreptest.pl
@@ -0,0 +1,9 @@
+:- module('spec', [trace_expression/2, match/2]).
+:- use_module(monitor('deep_subdict')).
+match(Event, enq(Val)) :- deep_subdict(_{'args':[Val],'name':"enqueue",'event':"func_pre"}, Event).
+match(Event, deq(Val)) :- deep_subdict(_{'res':Val,'name':"dequeue",'event':"func_post"}, Event).
+match(Event, deq) :- match(Event, deq(_)).
+match(Event, relevant) :- match(Event, enq(_)).
+match(Event, relevant) :- match(Event, deq).
+match(_, any).
+trace_expression('Main', Main) :- Main=(((relevant>>clos(var(val, ((enq(var(val)):eps)*((((deq>>((deq(var(val)):eps)*1));1))/\((star((enq(var(val)):eps))*(deq:eps))|Main))))));1)).
