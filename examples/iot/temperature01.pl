708fda4ce6172589ab209f77b45ea730207af824
davideancona <davide.ancona@unige.it>
2019-06-04 18:35:32 +0200

    exemples

--- examples/iot/temperature01.pl
+++
@@ -1,5 +0,0 @@
-:- module('spec', [trace_expression/2, match/2]).
-:- use_module(monitor('deep_subdict')).
-match(_event, temp_sensor_in_range(Min, Max)) :- deep_subdict(_{'temperature':Temp}, _event), ','((Min=<Temp), (Temp=<Max)).
-match(_, any).
-trace_expression('Main', Main) :- Main=app(CheckRange, [22, 24]), CheckRange=gen(['min', 'max'], star((temp_sensor_in_range(var(min), var(max)):eps))).
